6bddf4204928cf1256724865384dcfc3cac695c5
Suraj Gurung <sgrg007@gmail.com>
2020-05-23 00:53:46 -0500

    added dispalyTail

--- src/com/test/TestLogin.java
+++ src/com/test/TestLogin.java
@@ -3,5 +3,12 @@ package com.test;
 public class TestLogin {
     public static void main(String[] args) {
         System.out.println("suraj gurung");
+        TestLogin testLogi=new TestLogin();
+        testLogi.displaytail();
     }
+
+    public void displaytail(){
+        System.out.println("print from tail");
+    }
+
 }
