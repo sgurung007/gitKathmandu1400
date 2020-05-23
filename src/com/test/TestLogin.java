c6505578687023a07cd4f31ccedeead1e8bc515c
Suraj Gurung <sgrg007@gmail.com>
2020-05-23 01:00:51 -0500

    added displau wing

--- src/com/test/TestLogin.java
+++ src/com/test/TestLogin.java
@@ -5,10 +5,15 @@ public class TestLogin {
         System.out.println("suraj gurung");
         TestLogin testLogi=new TestLogin();
         testLogi.displaytail();
+        testLogi.displayWing();
     }
 
     public void displaytail(){
         System.out.println("print from tail");
+    }
+
+    public void displayWing(){
+        System.out.println("print from wing");
     }
 
 }
